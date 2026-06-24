640ee80894fac5a0c35ebcae6c3c78df57faa63f
Zafiza1 <user@Zafi.localdomain>
2026-06-24 15:56:11 +0700

    joss

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
     }
   },
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
-  assetsInclude: ['**/*.svg', '**/*.csv'],
+  assetsInclude: ['**/*.svg', '**/*.csv', '**/*.mp4', '**/*.webp'],
   build: {
     // Performance optimizations
     minify: 'terser',
