d8142ff9c5ea3f0f04b2eea2e673682e1854b97d
Zafiza1 <user@Zafi.localdomain>
2026-07-29 14:36:57 +0700

    update product, product and service dan admin 2

--- Backend/database/seeders/ProductCategorySeeder.php
+++ Backend/database/seeders/ProductCategorySeeder.php
@@ -20,12 +20,16 @@ class ProductCategorySeeder extends Seeder
         $categories = [
             ['main_category' => 'gas', 'slug' => 'industrial-medical', 'order' => 0],
             ['main_category' => 'gas', 'slug' => 'speciality-mixed', 'order' => 1],
-            ['main_category' => 'package', 'slug' => 'color-code', 'order' => 0],
-            ['main_category' => 'package', 'slug' => 'package', 'order' => 1],
-            ['main_category' => 'package', 'slug' => 'assist-gas', 'order' => 2],
-            ['main_category' => 'package', 'slug' => 'cryogenic-transport', 'order' => 3],
-            ['main_category' => 'package', 'slug' => 'regulator-valves', 'order' => 4],
-            ['main_category' => 'package', 'slug' => 'medical-gas-equipment', 'order' => 5],
+            ['main_category' => 'package', 'slug' => 'cradle-2x2', 'order' => 0],
+            ['main_category' => 'package', 'slug' => 'cradle-3x2', 'order' => 1],
+            ['main_category' => 'package', 'slug' => 'cradle-3x3', 'order' => 2],
+            ['main_category' => 'package', 'slug' => 'cradle-4x4', 'order' => 3],
+            ['main_category' => 'package', 'slug' => 'cylinder', 'order' => 4],
+            ['main_category' => 'package', 'slug' => 'cryogenic-dewars', 'order' => 5],
+            ['main_category' => 'package', 'slug' => 'vessel-gas-liquid', 'order' => 6],
+            ['main_category' => 'package', 'slug' => 'microbulk-tank', 'order' => 7],
+            ['main_category' => 'package', 'slug' => 'iso-tank', 'order' => 8],
+            ['main_category' => 'package', 'slug' => 'lorry-tank', 'order' => 9],
         ];
 
         $locales = LocaleReader::load();
