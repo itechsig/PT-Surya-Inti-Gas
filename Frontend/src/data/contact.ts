ec9109463f9500ff03e6e7b34a79ff21f00c139b
Zafiza1 <zafiza96@gmail.com>
2026-09-08 13:14:51 +0700

    Perbaikan Link Sosmed

--- Frontend/src/data/contact.ts
+++ Frontend/src/data/contact.ts
@@ -51,7 +51,7 @@ export const OFFICES: Record<Office['id'], Office> = {
 export const PRIMARY_OFFICE = OFFICES.sidoarjo;
 
 export const SOCIAL = {
-  instagram: 'https://www.instagram.com/surya.inti.gas?igsh=MXM3czQyOWx5ZjNzYw==',
+  instagram: 'https://www.instagram.com/suryaintigas?igsh=MXM3czQyOWx5ZjNzYw==',
   tiktok: 'https://www.tiktok.com/@surya.inti.gas?_r=1&_t=ZS-97WlfSPPexY',
   facebook: 'https://www.facebook.com/share/18bnUqF8GG/',
 };
